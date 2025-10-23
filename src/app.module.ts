8787800a9ec3c868526129649e0706e9a029745a
andrewprasetya-k <andrewprasetya.k@gmail.com>
2025-10-23 15:15:27 +0700

    import mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    // koneksi ke MongoDB
+    MongooseModule.forRoot('mongodb://localhost:27017/nestdb')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
