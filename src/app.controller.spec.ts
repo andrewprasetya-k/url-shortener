e37c6a612bfd6b8a48f94ce09ac61d6f2f8e6469
andrewprasetya-k <andrewprasetya.k@gmail.com>
2025-10-24 10:15:39 +0700

    refactor & mv directory

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controller/Controller';
+import { AppService } from './service/Service';
 
 describe('AppController', () => {
   let appController: AppController;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controller/Controller';
+import { AppService } from './service/Service';
 
 @Module({
   imports: [
