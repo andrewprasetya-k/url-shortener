ff33b3dd39b704aab19c19e9435f25348cc854bd
andrewprasetya-k <andrewprasetya.k@gmail.com>
2025-10-28 15:39:04 +0700

    stricter url checking

--- backend/src/Dto/CreateUrlDto.ts
+++ backend/src/Dto/CreateUrlDto.ts
@@ -1,7 +1,15 @@
-import { IsNotEmpty, IsUrl } from 'class-validator';
+import { IsNotEmpty, IsString, IsUrl, IsOptional } from 'class-validator';
 
 export class CreateUrlDto {
-  @IsUrl()
+  @IsOptional()
+  @IsString({ message: 'URL name must be a string' })
+  urlName?: string;
+
+  @IsUrl({
+    require_protocol: true,
+    require_valid_protocol: true,
+    protocols: ['http', 'https']
+  }, { message: 'Original URL must be a valid URL with http:// or https://' })
   @IsNotEmpty({ message: 'Original URL should not be empty' })
   originalUrl: string;
 }
