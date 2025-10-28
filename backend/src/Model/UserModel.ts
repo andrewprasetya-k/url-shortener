14ea27bb8d8f700876f34e43c9e6986ee8eda49f
andrewprasetya-k <andrewprasetya.k@gmail.com>
2025-10-28 13:10:47 +0700

    added password hashisng

--- backend/src/Model/UserModel.ts
+++ backend/src/Model/UserModel.ts
@@ -2,6 +2,7 @@ import * as AutoIncrementFactory from 'mongoose-sequence';
 import { Connection } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { InjectConnection } from '@nestjs/mongoose';
+import * as bycrypt from 'bcrypt';
 
 export type UserDocument = User & Document;
 
@@ -21,6 +22,12 @@ export class User {
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
+
+if (!UserSchema.methods.validatePassword) {
+  UserSchema.methods.validatePassword = async function (password: string): Promise<boolean> {
+    return bycrypt.compare(password, this.password);
+  };
+}
 
 export const applyAutoIncrement = (connection: Connection) => {
   const AutoIncrement = AutoIncrementFactory(connection);
