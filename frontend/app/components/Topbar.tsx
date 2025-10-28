09ce5786205a97c278e2b9700ffc3eea8eaa235b
andrewprasetya-k <andrewprasetya.k@gmail.com>
2025-10-28 11:23:56 +0700

    fixed z size topbar

--- frontend/app/components/Topbar.tsx
+++ frontend/app/components/Topbar.tsx
@@ -2,7 +2,7 @@
 
 export default function Topbar({title}:{title:string}) {
   return (
-    <header className="w-full shadow-md p-6 bg-white">
+    <header className="w-full p-6 bg-white fixed z-10">
         <h1 className="text-xl font-bold text-gray-700">{title}</h1>
     </header>
   );
