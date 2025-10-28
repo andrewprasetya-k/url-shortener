9717a60358de0b04d86d4deebe9a88036e00371e
andrewprasetya-k <andrewprasetya.k@gmail.com>
2025-10-28 11:38:03 +0700

    ignore sidebar and topbar in login page

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -8,20 +8,24 @@ import { sidebar } from './config';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname() || '';
   const hideSidebar = pathname === '/login';
+  const hideTopbar = pathname === '/login';
 
-  const pageTitle = pathname
-  .split('/')
-  .filter(Boolean)
-  .map(segment => segment.charAt(0).toUpperCase() + segment.slice(1))
-  .join(' / ') || 'Home';
+  const pageTitle =
+    pathname
+      .split('/')
+      .filter(Boolean)
+      .map(segment => segment.charAt(0).toUpperCase() + segment.slice(1))
+      .join(' / ') || 'Home';
 
   return (
     <html lang="en">
-      <body className="antialiased bg-gray-50 flex min-h-screen">
+      <body className="antialiased bg-yellow-50 flex min-h-screen">
         {!hideSidebar && <Sidebar />}
         <div className={`flex-1 flex flex-col ${!hideSidebar ? sidebar.marginLeft : ''}`}>
-          {!hideSidebar && <Topbar title={pageTitle}/>}
-          <main className='mt-16'>{children}</main>
+          {!hideTopbar && <Topbar title={pageTitle} />}
+          <main className={`${!hideTopbar ? 'mt-16' : ''}`}>
+            {children}
+          </main>
         </div>
       </body>
     </html>
