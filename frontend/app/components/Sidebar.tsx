1250de1895ba1df9ee30e7ced95a3ae305ffd87f
andrewprasetya-k <andrewprasetya.k@gmail.com>
2025-10-30 10:15:39 +0700

    mv logout button

--- frontend/app/components/Sidebar.tsx
+++ frontend/app/components/Sidebar.tsx
@@ -36,27 +36,32 @@ export default function Sidebar() {
 
       {/* Sidebar */}
       <aside
-        className={`fixed top-0 left-0 h-full text-gray-700 ${sidebar.width} p-4 py-6 space-y-6 transform transition-transform duration-300 z-40
+        className={`fixed top-0 left-0 h-full text-gray-700 ${sidebar.width} p-4 py-6 transform transition-transform duration-300 z-40 flex flex-col justify-between
         ${open ? 'translate-x-0' : '-translate-x-full sm:translate-x-0'}`}      >
-        <h2 className="text-2xl font-bold pl-2 mb-10">Url Shortener</h2>
+        <div>
+          <h2 className="text-2xl font-bold pl-2 mb-6">Url Shortener</h2>
 
-        <nav className="space-y-3">
-          <Link
-            href="/dashboard"
-            className="flex items-center gap-2 p-2 rounded bg-gray-200 transition"
-          >
-            <LayoutDashboard size={18} /> Dashboard
-          </Link>
+          <nav className="space-y-3">
+            <Link
+              href="/dashboard"
+              className="flex items-center gap-2 p-2 rounded bg-gray-200 transition"
+            >
+              <LayoutDashboard size={18} /> Dashboard
+            </Link>
 
-            <button
+          </nav>
+        </div>
+
+        <div>
+          <button
             type="button"
             onClick={logout}
             aria-label="Logout"
-            className="w-full text-left flex items-center gap-2 p-2 rounded hover:bg-gray-200 transition"
-            >
+            className="w-full text-left text-white bg-red-600 flex items-center gap-2 p-2 rounded hover:bg-red-700 transition cursor-pointer"
+          >
             <LogOut size={18} /> Logout
-            </button>
-        </nav>
+          </button>
+        </div>
       </aside>
     </>
   );
